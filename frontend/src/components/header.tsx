e6754797462718e7608633ac5f13e3efc2ea6014
nrzull <nrzull@tuta.io>
2019-07-10 16:04:09 +0500

    add header component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+import "./header.css";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      MTA:SA <span>Masterlist</span>
+    </div>
+  );
+}
+
+export { Header };
