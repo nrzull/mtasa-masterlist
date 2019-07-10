0821b3210fae70fa0a9d1cadd62477ab889d5438
nrzull <nrzull@tuta.io>
2019-07-10 16:04:54 +0500

    use Body and Header components in App

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,16 @@ import "@/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Header } from "@/components/header";
+import { Body } from "@/components/body";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
